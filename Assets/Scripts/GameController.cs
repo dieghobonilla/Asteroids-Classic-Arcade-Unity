88e37290fcfc53436f12f7f06542d13fbdcd46c5
Diego <dieghobonilla@gmail.com>
2020-11-28 19:38:00 +0000

    WIP, GameController and UIController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class GameController : MonoBehaviour
+{
+    public static event Action<int> OnPlayerDestroyed = delegate { };
+    public static event Action<int> OnLevelCleared = delegate { };
+    public static event Action OnGameStarted = delegate { };
+    public static event Action OnGameOver = delegate { };
+
+    [SerializeField] private int _initialPlayerShips = 3;
+
+    [SerializeField] private Player _player;
+    [SerializeField] private EnemiesFactory _enemiesFactory;
+
+    // Game settings
+    public static Camera MainCamera { get; private set; }
+    public static Player Player { get; private set; }
+
+    public static int PlayerShips;
+    private Coroutine _spawnUFOsCoroutine;
+
+    private void Awake()
+    {
+        MainCamera = Camera.main;
+        Assert.IsNotNull(MainCamera);
+
+        GameSettings.SetScreenLimits(MainCamera);
+    }
+
+    private void OnEnable()
+    {
+        Player.OnPlayerDestroyed += PlayerDestroyed;
+    }
+
+    private void OnDisable()
+    {
+        Player.OnPlayerDestroyed -= PlayerDestroyed;
+    }
+
+    private void Start()
+    {
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        Player = _player;
+        PlayerShips = _initialPlayerShips;
+        LoadNewLevel();
+
+        OnGameStarted?.Invoke();
+    }
+
+    private void PlayerDestroyed()
+    {
+        PlayerShips--;
+        OnPlayerDestroyed?.Invoke(PlayerShips);
+
+        if (PlayerShips <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        StopCoroutine(_spawnUFOsCoroutine);
+       
+        OnGameOver?.Invoke();
+    }
+
+    private void LoadNewLevel(int level = 1)
+    {
+        _enemiesFactory.LoadNewLevel(level);
+
+        if (_spawnUFOsCoroutine != null)
+        {
+            StopCoroutine(_spawnUFOsCoroutine);
+        }
+        
+        _spawnUFOsCoroutine = StartCoroutine(SpawnUFOsCoroutine());
+       
+        IEnumerator SpawnUFOsCoroutine()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(3);
+                _enemiesFactory.SpawnBigUFO(1);
+
+                yield return new WaitForSeconds(3);
+                _enemiesFactory.SpawnSmallUFO(1);
+
+                yield return new WaitForSeconds(5);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private Text _level;
+    [SerializeField] private Text _playerShips;
+    [SerializeField] private Text _score;
+    
+    [SerializeField] private GameObject _gameOverPanel;
+
+    private int _currentScore;
+    
+    private void Awake()
+    {
+        HideGameOverPanel();
+    }
+
+    private void OnEnable()
+    {
+        GameController.OnGameStarted += GameStarted;
+        GameController.OnGameOver += GameOver;
+        GameController.OnLevelCleared += LevelCleared;
+        GameController.OnPlayerDestroyed += PlayerDestroyed;
+        Enemy.OnEnemyDestroyed += EnemyDestroyed;
+    }
+
+    private void OnDisable()
+    {
+        GameController.OnGameStarted -= GameStarted;
+        GameController.OnGameOver -= GameOver;
+        GameController.OnLevelCleared -= LevelCleared;
+        GameController.OnPlayerDestroyed -= PlayerDestroyed;
+        Enemy.OnEnemyDestroyed -= EnemyDestroyed;
+    }
+
+    private void Start()
+    {
+        _currentScore = 0;
+    }
+
+    private void GameStarted()
+    {
+        HideGameOverPanel();
+    }
+
+    private void HideGameOverPanel()
+    {
+        _gameOverPanel.SetActive(false);
+    }
+    private void GameOver()
+    {
+        _gameOverPanel.SetActive(true);
+    }
+    
+    private void LevelCleared(int nextLevel)
+    {
+        _level.text = $"Level: {nextLevel:0000}";
+    }
+
+    private void PlayerDestroyed(int ships)
+    {
+        _playerShips.text = $"Ships: {ships:0000}";
+    }
+    
+    private void EnemyDestroyed(Enemy enemy, Queue<Enemy> arg2)
+    {
+        _currentScore += enemy.KillPoints;
+        _score.text = $"Score: {_currentScore:0000}";
+    }
+}
